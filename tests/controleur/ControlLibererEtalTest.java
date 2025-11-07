9b3d055576d6a1b7070d2e4fa8f7fc7bf5b82b35
Darksword333 <mazetgabriel@gmail.com>
2025-11-07 15:29:57 +0100

    Unit Test begin ControlLibererEtal

---
+++ tests/controleur/ControlLibererEtalTest.java
@@ -0,0 +1,39 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlLibererEtalTest {
+	
+	private ControlEmmenager cm;
+	private ControlTrouverEtalVendeur cev;
+	private ControlVerifierIdentite cv;
+	private ControlPrendreEtal cp;
+	private ControlLibererEtal cl;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Village village = new Village("LeVillage", 10, 1);
+		village.setChef(new Chef("LeChef", 1, village));
+		cm = new ControlEmmenager(village);
+		cv = new ControlVerifierIdentite(village);
+		cp = new ControlPrendreEtal(cv, village);
+		cev = new ControlTrouverEtalVendeur(village);
+		cl = new ControlLibererEtal(cev);
+	}
+
+	@Test
+	void testIsVendeur() {
+		cm.ajouterGaulois("Bonemine", 1);
+		cp.prendreEtal("Bonemine", "fleurs", 10);
+		assertTrue(cl.isVendeur("Bonemine"));
+		assertFalse(cl.isVendeur("Intru"));
+		assertFalse(cl.isVendeur("LeChef"));
+	}
+
+}
